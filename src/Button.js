60ef6e6f3a384214fdbffa0afe3c2d93ab89927f
heejaelee <hj.lee2@stickint.kr>
2022-12-09 11:00:34 +0900

    useState

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,8 @@
-// import PropTypes from ''
 import style from './Button.module.css'
 
 function Button({text}) {
   return (
-    // css 파일명이 ~.module.css가 아니라면 아래 방식으로 className가 작성되지 않는다.
+    // css 파일명이 ~.module.css가 아니라면 아래 방식으로 className이 작성되지 않는다.
     <button className={style.btn}>{text}</button>
   )
 }
