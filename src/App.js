fb27dda4dff1a27f3d8b649337de62f8f028e922
heejaelee <hj.lee2@stickint.kr>
2022-12-09 13:33:01 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import {useState, useEffect} from 'react'
-import Button from './Button'
-import styles from './App.module.css'
 
 function App() {
-  // 리액트는 새로운 UI 요소가 추가될 때 마다 화면을 새로고침(refresh)한다.
-  const [counter, setValue] = useState(0)
-  const [keyword, setKeyword] = useState('')
-
+  const [showing, setShowing] = useState(false)
   const onClick = () => {
-    setValue((counter) => counter + 1)
+    setShowing((prev) => !prev)
   }
 
-  const onChange = (event) => {
-    setKeyword(event.target.value)
+  function Hello () {
+    useEffect(() => {
+      console.log('Created!')
+    }, [])
+    return <h1>Hello</h1>
   }
-
-  console.log('I run all the time.')
-
-  // useEffect는 페이지가 렌더링 된 후 딱 1번만 실행된다.
-  useEffect(() => {
-    console.log('I run only once.')
-  }, [])
-
-  // counter 또는 keyword가 변할 때 마다 실행된다.
-  useEffect(() => {
-    console.log('I run when counter or keyword changes.', counter)
-  }, [counter, keyword])
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search here.."
-        value={keyword}
-        onChange={onChange} />
-
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text={"Component button"}/>
-      <button onClick={onClick}>Counter</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
---
+++ src/useState.js
@@ -0,0 +1,45 @@
+import {useState, useEffect} from 'react'
+import Button from './Button'
+import styles from './App.module.css'
+
+function useState() {
+  // 리액트는 새로운 UI 요소가 추가될 때 마다 화면을 새로고침(refresh)한다.
+  const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState('')
+
+  const onClick = () => {
+    setValue((counter) => counter + 1)
+  }
+
+  const onChange = (event) => {
+    setKeyword(event.target.value)
+  }
+
+  console.log('I run all the time.')
+
+  // useEffect는 페이지가 렌더링 된 후 딱 1번만 실행된다.
+  useEffect(() => {
+    console.log('I run only once.')
+  }, [])
+
+  // counter 또는 keyword가 변할 때 마다 실행된다.
+  useEffect(() => {
+    console.log('I run when counter or keyword changes.', counter)
+  }, [counter, keyword])
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search here.."
+        value={keyword}
+        onChange={onChange} />
+
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"Component button"}/>
+      <button onClick={onClick}>Counter</button>
+    </div>
+  );
+}
+
+export default useState;
